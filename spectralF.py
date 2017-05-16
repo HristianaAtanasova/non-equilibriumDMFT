9a3810e9a1e36c1e045266adac5014e39f307084
atanasovah@mail.tau.ac.il <atanasovah@mail.tau.ac.il>
2017-05-16 16:50:53 +0300

    renamed dmft_NCA.py to dmft_NCA_timedep.py and added plot scripts. the results for the explicit time propagation coincide with the time-independent case. the time arguments in the function tdiff were reversed

--- spectralF.py
+++ spectralF.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.fftpack import fft, ifft, fftshift, ifftshift
 
-tmax = 10
+tmax = 2
 dt = 0.01
 t = np.arange(0, tmax, dt)
 Cut = np.pi/dt
@@ -12,30 +12,35 @@ dw = np.pi/Cut
 fw = np.arange(-wmax, wmax, dw)
 w = np.arange(-7, 7, dw)
 
-Gles = np.loadtxt("Green_les_U=0.0_T=1_t=10.out").view(complex)
-Ggtr = np.loadtxt("Green_gtr_U=0.0_T=1_t=10.out").view(complex)
+Gles = np.zeros((4, len(t), len(t)), complex)
+Ggtr = np.zeros((4, len(t), len(t)), complex)
+
+les = 'Green_les_t={}_dt={}_i={}.out'
+gtr = 'Green_gtr_t={}_dt={}_i={}.out'
+
+# les = 'Green_fft_les_t={}_dt={}_i={}.out'
+# gtr = 'Green_fft_gtr_t={}_dt={}_i={}.out'
+
+for i in range(4):
+    Gles[i] = np.loadtxt(les.format(tmax,dt,i)).view(complex)
+    Ggtr[i] = np.loadtxt(gtr.format(tmax,dt,i)).view(complex)
+
+i= 0
+Gles = Gles[i, len(t)-1, ::-1]
+Ggtr = Ggtr[i, len(t)-1, ::-1]
 
 N = int(Cut/dt)
 Gret = np.zeros(N+1, complex)
 Gret[0:int(len(t))] = Ggtr + np.conj(Gles)
-Gret = np.imag(Gret) + 1j*np.real(Gret)
 
 fGret = fftshift(fft(Gret)) * dt/np.pi
-# fGret = np.imag(fGret) + 1j*np.real(fGret)
+fGret = np.imag(fGret) + 1j*np.real(fGret)
 a = int((N-len(w))/2)
 b = int((N+len(w))/2)
 
-# plt.figure(1)
-# plt.subplot(211)
-# plt.plot(w, np.real(fGret[a:b]), 'r--', w, np.imag(fGret[a:b]), 'b--')
 plt.plot(w, np.imag(fGret[a:b]))
-# plt.plot(fw, np.real(fGret), 'r--', fw, np.imag(fGret), 'b--')
-plt.ylabel('A(w)')
-plt.xlabel('w')
+plt.legend(loc='best')
+plt.ylabel('A($\omega$)')
+plt.xlabel('$\omega$')
 plt.grid()
-# plt.subplot(212)
-# plt.plot(t, np.real(Gret[0:int(len(t))]), 'r--', label='Green_ret_r')
-# plt.plot(t, np.imag(Gret[0:int(len(t))]), 'b--', label='Green_ret_i')
-# plt.legend()
-# plt.grid()
 plt.show()
