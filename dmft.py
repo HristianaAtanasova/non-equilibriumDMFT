6f052e256e0ec4dd8a523219cdb30d49e64e9395
atanasovah@mail.tau.ac.il <atanasovah@mail.tau.ac.il>
2017-09-06 17:09:31 +0300

    added separate files

---
+++ dmft.py
@@ -0,0 +1,117 @@
+# !/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import toml
+
+from datetime import datetime
+from scipy.fftpack import fft, ifft, fftfreq, fftshift, ifftshift
+
+import nca
+import hybridization
+
+import h5py
+import hdf5
+
+def runNCA(U, tmax, dt, Delta, iteration, output):
+    t  = np.arange(0, tmax, dt)
+    U_ = U * np.ones(t.shape[0], float)
+
+    hybsection = "dmft/iterations/{}/delta".format(iteration)
+    gfsection  = "dmft/iterations/{}/gf".format(iteration)
+
+    with h5py.File(output, "a") as h5f:
+        hdf5.save_green(h5f, hybsection, Delta, (t,t))
+
+    nca.solve(t, U_, output, hybsection, gfsection)
+
+    with h5py.File(output, "r") as h5f:
+        Green, _ = hdf5.load_green(h5f, gfsection)
+
+    return Green
+
+
+def run_dmft(U, T, mu, v_0, tmax, dt, dw, tol, solver, output, **kwargs):
+    t  = np.arange(0, tmax, dt)
+
+    msg = 'Starting DMFT loop for U = {} | Temperature = {} | time = {} | dt = {}'.format(U, T, tmax, dt)
+    print('-'*len(msg))
+    print(msg)
+    print('-'*len(msg))
+
+    start = datetime.now()
+
+    # gf indices: [gtr/les, up/down, state, time, time]
+    Green     = np.zeros((2, 2, len(t), len(t)), complex)
+    Green_old = np.zeros((2, 2, len(t), len(t)), complex)
+
+    # delta indices: [gtr/les, up/down, time, time]
+    np.seterr(over="ignore")
+    Delta = hybridization.genSemicircularHyb(T, mu, v_0, tmax, dt, dw)
+    np.seterr(over="warn")
+
+    if solver == "NCA":
+        Solver = runNCA
+    elif solver == "inchworm":
+        Solver = runInch
+    else:
+        raise Exception("solver {:s} not recognized".format(solver))
+
+    # DMFT self-consistency loop
+    diff = np.inf
+    iteration = 0
+    while diff > tol:
+        iteration += 1
+
+        Green_old = Green
+
+        Green = Solver(U, tmax, dt, Delta, iteration, output)
+
+        diff = np.amax(np.abs(Green_old - Green))
+
+        # antiferromagnetic self-consistency
+        Delta[:, 0, :, :] = v_0 * Green[:, 1, :, :] * v_0
+        Delta[:, 1, :, :] = v_0 * Green[:, 0, :, :] * v_0
+
+        msg = 'U = {}, iteration {}: diff = {} (elapsed time = {})'
+        print(msg.format(U, iteration, diff, datetime.now() - start))
+
+    msg = 'Computation finished after {} iterations and {} seconds'.format(iteration, datetime.now() - start)
+    print('-'*len(msg))
+    print(msg)
+    print('-'*len(msg))
+
+    return Green
+
+def main():
+    parser = argparse.ArgumentParser(description = "run dmft")
+    parser.add_argument("--output",   default = "output.h5")
+    parser.add_argument("--params",   default = "params.toml")
+    parser.add_argument("--savetxt",  action  = "store_true")
+    args = parser.parse_args()
+
+    with open(args.params, "r") as f:
+        params = toml.load(f)
+
+    params.update(vars(args))
+
+    with h5py.File(args.output, "a") as h5f:
+        for k,v in params.items():
+            h5f.create_dataset("dmft/params/" + k, data=v)
+
+    Green = run_dmft(**params)
+
+    if args.savetxt:
+        gtr_up   = 'gtr_up_U={}_T={}_t={}_dt={}.out'
+        les_up   = 'les_up_U={}_T={}_t={}_dt={}.out'
+        gtr_down = 'gtr_down_U={}_T={}_t={}_dt={}.out'
+        les_down = 'les_down_U={}_T={}_t={}_dt={}.out'
+
+        np.savetxt(gtr_up.format(*[params[x] for x in ["U", "T", "tmax", "dt"]]), Green[0, 0, 1].view(float), delimiter=' ')
+        np.savetxt(les_up.format(*[params[x] for x in ["U", "T", "tmax", "dt"]]), Green[1, 0, 1].view(float), delimiter=' ')
+        np.savetxt(gtr_down.format(*[params[x] for x in ["U", "T", "tmax", "dt"]]), Green[0, 1, 1].view(float), delimiter=' ')
+        np.savetxt(les_down.format(*[params[x] for x in ["U", "T", "tmax", "dt"]]), Green[1, 1, 1].view(float), delimiter=' ')
+
+if __name__ == "__main__":
+main()
---
+++ hdf5.py
@@ -0,0 +1,58 @@
+import numpy as np
+import h5py
+
+def h5_load(archive, section):
+    data = archive[section].value
+    if "__complex__" in archive[section].attrs:
+        data = data[...,0] + 1.0j * data[...,1]
+    return data
+
+def h5_save(archive, section, data):
+    if np.iscomplexobj(data):
+        archive.create_dataset(section, data = np.stack([data.real, data.imag], axis=-1))
+        archive[section].attrs.create("__complex__", 1)
+    else:
+        archive.create_dataset(section, data = data)
+
+def load_grid_object(archive, section):
+    data = h5_load(archive, section + "/data")
+
+    grids = []
+    for i, g in archive[section + "/grids/"].items():
+        grids += [g.value]
+
+    return data, grids
+
+def save_grid_object(archive, section, data, grids):
+    assert(len(grids) == len(data.shape))
+    h5_save(archive, section + "/data", data)
+    for i, g in enumerate(grids):
+        h5_save(archive, section + "/grids/{}".format(i), g)
+
+def load_green(archive, section):
+    data, grids = load_grid_object(archive, section + "/gtr_up")
+
+    t0 = grids[0]
+    t1 = grids[1]
+
+    Green = np.zeros((2, 2, len(t0), len(t0)), complex)
+
+    Green[0,0,:,:] = data
+
+    data, grids = load_grid_object(archive, section + "/les_up")
+    Green[1,0,:,:] = data
+
+    data, grids = load_grid_object(archive, section + "/gtr_down")
+    Green[0,1,:,:] = data
+
+    data, grids = load_grid_object(archive, section + "/les_down")
+    Green[1,1,:,:] = data
+
+    return Green, grids
+
+def save_green(archive, section, Green, grids):
+    #indices grt/less, up/down, time, times
+    save_grid_object(archive, section + "/gtr_up",   Green[0,0,:,:], grids)
+    save_grid_object(archive, section + "/les_up",   Green[1,0,:,:], grids)
+    save_grid_object(archive, section + "/gtr_down", Green[0,1,:,:], grids)
+save_grid_object(archive, section + "/les_down", Green[1,1,:,:], grids)
