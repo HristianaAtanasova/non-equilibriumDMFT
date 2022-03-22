added calculation of impurity coupled to two leads and fixed the error in the calculation of the lattice current

bethe_dmft.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
@@ -21,7 +21,7 @@ import arrange_times
 # import hdf5
 
 
-def runNCA(U, pumpA, T, G_0, tmax, dt, Delta, phonon, fermion, Lambda, dissBath, iteration, output):
+def runNCA(U, mu, pumpA, T, G_0, tmax, dt, Delta, phonon, fermion, Lambda, dissBath, iteration, output):
     t  = np.arange(0, tmax, dt)
 
     # hybsection = "dmft/iterations/{}/delta".format(iteration)
@@ -30,7 +30,7 @@ def runNCA(U, pumpA, T, G_0, tmax, dt, Delta, phonon, fermion, Lambda, dissBath,
     # with h5py.File(output, "a") as h5f:
     #     hdf5.save_green(h5f, hybsection, Delta, (t,t))
 
-    Green = nca.solve(t, U, pumpA, T, G_0, phonon, fermion, Lambda, dissBath, output, Delta)
+    Green = nca.solve(t, U, mu, mu, pumpA, T, G_0, phonon, fermion, Lambda, dissBath, output, Delta)
 
     # with h5py.File(output, "r") as h5f:
     #     Green, _ = hdf5.load_green(h5f, gfsection)
@@ -43,7 +43,7 @@ def runInch(U, tmax, dt, Delta):
 def run_dmft(U, T, pumpA, probeA, mu, v_0, tmax, dt, dw, tol, solver, phonon, fermion, Lambda, wC, t_diss_end, pumpOmega, t_pump_start, t_pump_end, probeOmega, t_probe_start, t_probe_end, lattice_structure, output, **kwargs):
     t  = np.arange(0, tmax, dt)
 
-    msg = 'Starting DMFT loop for U = {} | Temperature = {} | mu = {} | phonon = {} | fermion = {} | Lambda = {} | time = {} | dt = {}'.format(U, T, mu, phonon, fermion, Lambda, tmax, dt)
+    msg = 'Starting DMFT loop for U = {} | F = {} | mu = {} | phonon = {} | fermion = {} | Lambda = {} | time = {} | dt = {}'.format(U, pumpA, mu, phonon, fermion, Lambda, tmax, dt)
     print('-'*len(msg))
     print(msg)
     print('-'*len(msg))
@@ -63,9 +63,9 @@ def run_dmft(U, T, pumpA, probeA, mu, v_0, tmax, dt, dw, tol, solver, phonon, fe
 
     # delta indices: [gtr/les, up/down, time, time]
     hybridization.genSemicircularHyb(T, mu, v_0, wC, tmax, dt, dw)
-    # hybridization.genWideBandHyb(T, mu, tmax, dt, dw)
+    # hybridization.genWideBandHyb(T, mu, wC, tmax, dt, dw)
     # timedep_hybridization.genWideBandHyb(T, mu, tmax, dt, dw)
-    loaded = np.load('Delta.npz')
+    loaded = np.load('Delta_mu={}_T={}_dt={}.npz'.format(mu, T, dt))
     Delta = loaded['D']
 
     if phonon == 1:
@@ -114,10 +114,12 @@ def run_dmft(U, T, pumpA, probeA, mu, v_0, tmax, dt, dw, tol, solver, phonon, fe
         # plt.savefig('hybridization_bethe.pdf')
         # plt.close()
 
-        Green = Solver(U, pumpA, T, G_0, tmax, dt, Delta, phonon, fermion, Lambda, dissBath, iteration, output)
+        Green = Solver(U, mu, pumpA, T, G_0, tmax, dt, Delta, phonon, fermion, Lambda, dissBath, iteration, output)
 
         diff = np.amax(np.abs(Green_old - Green))
         # diff = 0
+
+        # Delta = v * Green
 
         # antiferromagnetic self-consistency
         Delta[:, 1] = v * Green[:, 0]
@@ -133,25 +135,25 @@ def run_dmft(U, T, pumpA, probeA, mu, v_0, tmax, dt, dw, tol, solver, phonon, fe
         Delta = arrange_times.keldysh_to_real_time(contour_Delta, t, tmax)
         Green = arrange_times.keldysh_to_real_time(contour_Green, t, tmax)
 
-        plt.matshow(contour_Delta[0].real)
-        plt.colorbar()
-        plt.savefig('bethe_Delta_real.pdf')
-        plt.close()
+        # plt.matshow(contour_Delta[0].real)
+        # plt.colorbar()
+        # plt.savefig('bethe_Delta_real.pdf')
+        # plt.close()
 
-        plt.matshow(contour_Delta[0].imag)
-        plt.colorbar()
-        plt.savefig('bethe_Dalta_imag.pdf')
-        plt.close()
+        # plt.matshow(contour_Delta[0].imag)
+        # plt.colorbar()
+        # plt.savefig('bethe_Dalta_imag.pdf')
+        # plt.close()
 
-        plt.matshow(contour_Green[0].real)
-        plt.colorbar()
-        plt.savefig('bethe_Green_real.pdf')
-        plt.close()
+        # plt.matshow(contour_Green[0].real)
+        # plt.colorbar()
+        # plt.savefig('bethe_Green_real.pdf')
+        # plt.close()
 
-        plt.matshow(contour_Green[0].imag)
-        plt.colorbar()
-        plt.savefig('bethe_Green_imag.pdf')
-        plt.close()
+        # plt.matshow(contour_Green[0].imag)
+        # plt.colorbar()
+        # plt.savefig('bethe_Green_imag.pdf')
+        # plt.close()
 
         print('\n')
         msg = 'U = {}, iteration {}: diff = {} (elapsed time = {})'

calculate_G.py
@@ -18,12 +18,10 @@ def heaviside(x):
         return 0.5
     return 0 if x < 0 else 1
 
-#Path = os.getcwd() + '/U{}_dt{}_tmax{}'
-Path = os.getcwd() 
-
 # set up parameters
-#path = Path.format(U, dt, tmax)
-path = Path
+# Path = os.getcwd() + '/U{}_dt{}_tmax{}'
+# path = Path.format(U, dt, tmax)
+path = os.getcwd()
 parser = argparse.ArgumentParser(description = "run dmft")
 parser.add_argument("--params",   default = path + "/run.toml")
 args = parser.parse_args()
@@ -35,107 +33,132 @@ params.update(vars(args))
 
 U = params['U']
 T = params['T']
-dt = params['dt'] 
+# T = 0.05
+F = params['pumpA']
+mu = params['mu']
+# dt = params['dt'] 
 tmax = params['tmax']
 pumpOmega = params['pumpOmega']
 
-t  = np.arange(0, tmax, dt)
 msg = 'Executing U = {} | tmax = {}'
 print(msg.format(U, tmax))
 
-# load Greens functions
-#Green_path = path + '/Green_T={}.npz'
-Green_path = 'Green_T={}.npz'
-loaded = np.load(Green_path.format(T))
-t_ = loaded['t']
-dt = params['dt']
-Green = loaded['Green']
+# dts = [0.01, 0.005, 0.0025]
+dts = [0.01]
 
-# fft parameters
-dt = dt
-Cut_w = 2 * np.pi / dt
-dw = dt
-Cut_t = 2 * np.pi / dw
+for dt in dts:
+    print(dt)
+    # load Greens functions
+    Green = 'Green_U={}_F={}_mu1={}_mu2={}_T={}_dt={}.npz'
+    loaded = np.load(Green.format(U, F, mu, mu, T, dt))
+    Green = loaded['Green']
+    t_  = np.arange(0, tmax, dt)
 
-ft = np.arange(-Cut_t/2, Cut_t/2, dt)
-fw = np.arange(-Cut_w/2, Cut_w/2, dw)
+    # fft parameters
+    dt = dt
+    Cut_w = 2 * np.pi / dt
+    dw = dt
+    Cut_t = 2 * np.pi / dw
+    
+    ft = np.arange(-Cut_t/2, Cut_t/2, dt)
+    fw = np.arange(-Cut_w/2, Cut_w/2, dw)
+    
+    N_w = len(fw)
+    N_t = len(ft)
+    
+    # convert to plotting time and frequency domain
+    wmin = -10.0
+    wmax = -wmin
+    w = np.arange(wmin, wmax, dw)
+    w_start = int((N_w/2 + int(wmin/dw)))
+    w_end = int((N_w/2 + int(wmax/dw)))
+    
+    tmin = -tmax
+    t = np.arange(tmin, tmax, dt)
+    t_start = int((N_t/2 + int(tmin/dt)))
+    t_end = int((N_t/2 + int(tmax/dt)))
+    
+    # period = 2*np.pi/pumpOmega
+    period = dt
+    t_period = np.arange(params['tmax'] - period, params['tmax'], dt)
+    
+    G_p = np.zeros((len(t_period), 4, len(t)), complex)           # second index: Gles | Ggtr | Gret | Gadv
+    
+    Heaviside_ret = np.zeros(len(t))
+    Heaviside_adv = np.zeros(len(t))
+    for t_i in range(len(t)):
+        Heaviside_ret[t_i] = heaviside(t[t_i])
+        Heaviside_adv[-t_i] = heaviside(t[t_i])
+        Heaviside_adv[0] = heaviside(-t[0])
+    
+    # period averaged Greens functions
+    steps = 2
+    for i in range(0, len(t_period), steps):
+        t_p = np.arange(0, t_period[i], dt)
+    
+        t_0 = int(len(t_))
+        t_lower = t_0 - int(len(t_p)) 
+        t_upper = t_0 + int(len(t_p))
+    
+        G_p[i, 0, t_lower:t_0] = 1j * (Green[1, 0, 0:len(t_p), len(t_p)-1] + Green[1, 1, 0:len(t_p), len(t_p)-1]) #U=10
+        G_p[i, 0, t_0:t_upper] = 1j * (Green[1, 0, len(t_p)-1,  0:len(t_p)][::-1] + Green[1, 1, len(t_p)-1,  0:len(t_p)][::-1])
+    
+        G_p[i, 1, t_lower:t_0] = 1j * (Green[0, 0, 0:len(t_p), len(t_p)-1] + Green[0, 1, 0:len(t_p), len(t_p)-1]) #U=10
+        G_p[i, 1, t_0:t_upper] = 1j * (Green[0, 0, len(t_p)-1,  0:len(t_p)][::-1] + Green[0, 1, len(t_p)-1,  0:len(t_p)][::-1])
+    
+        ##################################################################################################################################
+        G_p[i, 0] = G_p[i,0] # Gles(t1,t2) 
+        G_p[i, 1] = -np.conj(G_p[i,1]) # Ggtr(t1,t2) 
+    
+        # Gret = -Theta(t)*(Ggtr - Gles)
+        G_p[i, 2] = -Heaviside_ret*(G_p[i,1] + G_p[i,0])
+    
+        # Gadv = -Theta(-t)*(Ggtr - Gles)
+        G_p[i, 3] = Heaviside_adv*(G_p[i,1] + G_p[i,0])
+    
+    G = np.sum(G_p,axis=0) / len(t_period)
+    
+    # # save energy current as .out file
+    # np.savetxt(path +'/Gles.out', G[0].view(float))
+    # np.savetxt(path +'/Ggtr.out', G[1].view(float))
+    # np.savetxt(path +'/Gret.out', G[2].view(float))
+    # np.savetxt(path +'/Gadv.out', G[3].view(float))
+    
+    #######################################################################################################################################
+    # fft of Greens functions
+    G_N = np.zeros((4, N_t), complex) #  Gles | Ggtr | Gret | Gadv
+    G_N[:, t_start:t_end] = G
+    
+    fG = ifftshift(fft(fftshift(G_N))) * dt / np.pi
+    # A = (-fG[2] + fG[3]) 
+    # A = (fG[2] - fG[3]) 
+    A = -(fG[1] + fG[0]) 
+    A_les = fG[0]
+    A_gtr = fG[1]
+    # f = np.imag(fG[0]) / (np.imag(A))
+    f = np.imag(fG[0]) / (np.imag(fG[0]) + np.imag(fG[1]))
 
-N_w = len(fw)
-N_t = len(ft)
+    I = np.imag(A_les[w_start:w_end]) - fermi_function(w, 0, 1.0/T) * np.imag(A[w_start:w_end])        
 
-# convert to plotting time and frequency domain
-wmin = -15.0
-wmax = 15.0
-w = np.arange(wmin, wmax, dw)
-w_start = int((N_w/2 + int(wmin/dw)))
-w_end = int((N_w/2 + int(wmax/dw)))
+    plt.plot(t, np.real(G[2] - G[3]), label = 'G_ret-G_adv_real')
+    plt.plot(t, np.imag(G[2] - G[3]), '--', label = 'G_ret-G_adv_imag')
+    plt.plot(t, -np.real(G[0] + G[1]), label = 'G_les-G_gtr_real')
+    plt.plot(t, -np.imag(G[0] + G[1]), '--', label = 'G_les-G_gtr_imag')
+    plt.legend()
+    plt.show()
+    
+    # plt.plot(fw[w_start:w_end], np.imag(A[w_start:w_end]), label = 'G_ret+G_adv')
+    # plt.plot(fw[w_start:w_end], np.imag(fG[0,w_start:w_end] + fG[1,w_start:w_end]), label = 'G_les+G_gtr')
+    plt.plot(fw[w_start:w_end], np.imag(A_les[w_start:w_end]), label = 'G_les_dt={}'.format(dt))
+    # plt.plot(fw[w_start:w_end], np.imag(A_gtr[w_start:w_end]), label = 'G_gtr_dt={}'.format(dt))
+    # plt.plot(fw[w_start:w_end], fermi_function(w, 0, 1.0/T) * np.imag(A[w_start:w_end]), label = '-fermi*(G_ret-G_a)_dt={}'.format(dt))
+    plt.plot(fw[w_start:w_end], -f[w_start:w_end] * np.imag(A[w_start:w_end]), label = '-fermi*(G_ret-G_a)_dt={}'.format(dt))
+    # plt.plot(fw[w_start:w_end], I, label = 'I_dt={}'.format(dt))
+    # plt.plot(fw[w_start:w_end], f[w_start:w_end], label = 'dt = {}'.format(dt))
+    # plt.plot(w, fermi_function(w, 0, 1.0/T), color='black')
 
-tmin = -tmax
-t = np.arange(tmin, tmax, dt)
-t_start = int((N_t/2 + int(tmin/dt)))
-t_end = int((N_t/2 + int(tmax/dt)))
-
-# period = 2*np.pi/pumpOmega
-period = dt
-t_period = np.arange(params['tmax'] - period, params['tmax'], dt)
-
-G_p = np.zeros((len(t_period), 4, len(t)), complex)           # second index: Gles | Ggtr | Gret | Gadv
-
-Heaviside_ret = np.zeros(len(t))
-Heaviside_adv = np.zeros(len(t))
-for t_i in range(len(t)):
-    Heaviside_ret[t_i] = heaviside(t[t_i])
-    Heaviside_adv[-t_i] = heaviside(t[t_i])
-    Heaviside_adv[0] = heaviside(-t[0])
-
-# period averaged Greens functions
-steps = 1
-for i in range(0, len(t_period), steps):
-    t_p = np.arange(0, t_period[i], dt)
-
-    t_lower = int(len(t_) - len(t_p)) + 1
-    t_0 = int(len(t) / 2)
-    t_upper = int(len(t_) + len(t_p))
-
-    G_p[i, 0, t_lower:t_0+1] = 1j * np.conj(Green[1, 0, 0:len(t_p), len(t_p)-1] + Green[1, 1, 0:len(t_p), len(t_p)-1]) #U=10
-    G_p[i, 0, t_0:t_upper] = 1j * np.conj(Green[1, 0, len(t_p)-1,  0:len(t_p)][::-1] + Green[1, 1, len(t_p)-1,  0:len(t_p)][::-1])
-
-    G_p[i, 1, t_lower:t_0+1] = 1j * (Green[0, 0, 0:len(t_p), len(t_p)-1] + Green[0, 1, 0:len(t_p), len(t_p)-1]) #U=10
-    G_p[i, 1, t_0:t_upper] = 1j * (Green[0, 0, len(t_p)-1,  0:len(t_p)][::-1] + Green[0, 1, len(t_p)-1,  0:len(t_p)][::-1])
-
-    ##################################################################################################################################
-    G_p[i, 0] = -np.conj(G_p[i,0]) # Gles(t1,t2) = -np.conj(Gles(t2,t1))
-    G_p[i, 1] = np.conj(G_p[i,1]) # Ggtr(t1,t2) = -(-np.conj(Ggtr(t2,t1)))
-
-    # Gret = -Theta(t)*(Ggtr - Gles)
-    G_p[i, 2] = -Heaviside_ret*(G_p[i,1] - G_p[i,0])
-
-    # Gadv = -Theta(-t)*(Ggtr - Gles)
-    G_p[i, 3] = -Heaviside_adv*(G_p[i,1] - G_p[i,0])
-
-G = np.sum(G_p,axis=0) / len(t_period)
-
-# save energy current as .out file
-np.savetxt(path +'/Gles.out', G[0].view(float))
-np.savetxt(path +'/Ggtr.out', G[1].view(float))
-np.savetxt(path +'/Gret.out', G[2].view(float))
-np.savetxt(path +'/Gadv.out', G[3].view(float))
-
-#######################################################################################################################################
-# fft of Greens functions
-G_N = np.zeros((4, N_t), complex) #  Gles | Ggtr | Gret | Gadv
-G_N[:, t_start:t_end] = G
-
-fG = ifftshift(fft(fftshift(G_N))) * dt/(2*np.pi)
-A = (fG[2]+fG[3]) / 2
-f = np.imag(fG[0]) / (2*np.imag(A))
-
-plt.plot(fw[w_start:w_end], np.imag(A[w_start:w_end]))
-# plt.plot(fw[w_start:w_end], np.imag(fG[0][w_start:w_end]))
-# plt.plot(fw[w_start:w_end], f[w_start:w_end])
-# plt.plot(w, fermi_function(w, 0, 1.0), color='red', label='fermi_dirac')
-# plt.legend()
-# plt.grid()
+plt.legend()
+plt.grid()
 # plt.ylim(-0.5,1.5)
-# plt.show()
-plt.savefig('A.pdf')
+plt.savefig('DOS_U={}_T={}.pdf'.format(U, T))
+# plt.savefig('fermi_U={}_T={}.pdf'.format(U, T))

calculate_current.py
@@ -8,34 +8,74 @@ import numpy as np
 import os
 import constant_electric_field
 
-def calculate_current(U, F, T, v_0, lattice_structure):
+def heaviside(x):
+    if x == 0:
+        return 0.5
+    return 0 if x < 0 else 1
+
+def calculate_current(U, F, mu1, mu2, T, dt, v_0, lattice_structure):
     # load Greens functions
-    Green = 'Green_U={}_F={}_T={}.npz'
-    loaded = np.load(Green.format(U, F, T))
-    t = loaded['t']
-    dt = t[1] - t[0]
+    Green = 'Green_U={}_F={}_mu1={}_mu2={}_T={}_dt={}.npz'
+    loaded = np.load(Green.format(U, F, mu1, mu2, T, dt))
+    t_ = loaded['t']
+    dt = t_[1] - t_[0]
+    tmax = len(t_) * dt
     Green = loaded['Green']
 
-    Delta = np.zeros((2, 2, len(t), len(t)), complex) # dynamical mean field
-    I = np.zeros((2, len(t)), complex) # lattice current; spin up/spin down | time
+    tmin = -tmax
+    t = np.arange(tmin, tmax, dt)
+    t_cut = 0
+
+    Delta = np.zeros((2, 2, len(t_), len(t_)), complex) # dynamical mean field
+    I = np.zeros((2, len(t_)), complex) # lattice current; spin up/spin down | time
+    G_p = np.zeros((4, len(t)), complex)           # second index: Gles | Ggtr | Gret | Gadv
 
     # v = electric_field.genv(pumpA, pumpOmega, params['t_pump_start'], params['t_pump_end'], probeA, probeOmega, params['t_probe_start'], params['t_probe_end'], params['v_0'], t, params['lattice_structure'])
-    v = constant_electric_field.genv(F, v_0, t, lattice_structure)
+    v = constant_electric_field.genv(F, v_0, t_, lattice_structure)
 
-    Delta[:, 0] = v * Green[:, 1]
+    Heaviside_ret = np.zeros(len(t))
+    Heaviside_adv = np.zeros(len(t))
+    for t_i in range(len(t)):
+        Heaviside_ret[t_i] = heaviside(t[t_i])
+        Heaviside_adv[-t_i] = heaviside(t[t_i])
+        Heaviside_adv[0] = heaviside(-t[0])
+
+    t_0 = int(len(t_))
+    t_p = np.arange(0, tmax-t_cut, dt)
+    t_lower = t_0 - int(len(t_p))
+    t_upper = t_0 + int(len(t_p))
+
+    G_p[0, t_lower:t_0] = 1j * (Green[1, 0, 0:len(t_p), len(t_p)-1] + Green[1, 1, 0:len(t_p), len(t_p)-1])
+    G_p[0, t_0:t_upper] = 1j * (Green[1, 0, len(t_p)-1,  0:len(t_p)][::-1] + Green[1, 1, len(t_p)-1,  0:len(t_p)][::-1])
+    G_p[1, t_lower:t_0] = 1j * (Green[0, 0, 0:len(t_p), len(t_p)-1] + Green[0, 1, 0:len(t_p), len(t_p)-1])
+    G_p[1, t_0:t_upper] = 1j * (Green[0, 0, len(t_p)-1,  0:len(t_p)][::-1] + Green[0, 1, len(t_p)-1,  0:len(t_p)][::-1])
+
+    G_p[0] = G_p[0] # Gles(t1,t2) 
+    G_p[1] = -np.conj(G_p[1]) # Ggtr(t1,t2) 
+    G_p[2] = -Heaviside_ret*(G_p[1] + G_p[0]) # Gret = -Theta(t)*(Ggtr - Gles)
+    G_p[3] = Heaviside_adv*(G_p[1] + G_p[0]) # Gadv = -Theta(-t)*(Ggtr - Gles)
+    
+    # plt.plot(t, -np.real(G_p[2] - G_p[3]), t, -np.imag(G_p[2] - G_p[3]), '--', label='-G_ret-G_adv_dt={}'.format(dt))
+    # plt.plot(t, np.real(G_p[0] + G_p[1]), t, np.imag(G_p[0] + G_p[1]), '--', label='G_les+G_gtr_dt={}'.format(dt))
+    # plt.plot(t, np.real(G_p[0]), t, np.imag(G_p[0]), '--', label='Green_les_dt={}'.format(dt))
+    # plt.plot(t, np.real(G_p[1]), t, np.imag(G_p[1]), '--', label='Green_gtr_dt={}'.format(dt))
+    # plt.legend()
+    # plt.show()
+    # plt.savefig('Greens_U={}_T={}.pdf'.format(U, T))
+
     Delta[:, 1] = v * Green[:, 0]
+    Delta[:, 0] = v * Green[:, 1]
 
-    spin = 0 
+    t = np.arange(0, tmax, dt)
+    # t_start = len(t)-100    
+    t_start = 0    
+    t_cut = len(t) - 0
+    # calculate particle current in the lattice
     for t1 in range(len(t)):
-        # calculate particle current in the lattice
-        I[0, t1] = dt * np.trapz(Delta[1, 0, t1, :t1] * Green[1, 0, t1, :t1])
-        I[1, t1] = dt * np.trapz(Delta[1, 1, t1, :t1] * Green[1, 1, t1, :t1])
-        # I[0, t1] = dt * np.trapz(Delta[1, spin, :t1, t1] * Green[1, spin, :t1, t1])
-        # I[1, t1] = dt * np.trapz(Delta[0, spin, :t1, t1] * Green[0, spin, :t1, t1])
-
-    # I[0] = Green[1, 0].diagonal()
-    # I[1] = Green[0, 1].diagonal()
-
+        # spin up 
+        I[1, t1] = dt * np.trapz(Delta[1, 1, :t1, t1-1] * (-Green[0, 1, :t1, t1-1] - Green[1, 1, :t1, t1-1])) + dt * np.trapz((Delta[1, 1, :t1, t1-1] + Delta[0, 1, :t1, t1-1]) * Green[1, 1, :t1, t1-1])
+        # spin down
+        I[0, t1] = dt * np.trapz(Delta[1, 0, :t1, t1-1] * (-Green[0, 0, :t1, t1-1] - Green[1, 0, :t1, t1-1])) + dt * np.trapz((Delta[1, 0, :t1, t1-1] + Delta[0, 0, :t1, t1-1]) * Green[1, 0, :t1, t1-1])
     return t, I
 
 def main():
@@ -50,16 +90,33 @@ def main():
 
     params.update(vars(args))
     
-    # Fs = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0]
-    Fs = [0.0]
+    U = params['U']
+    T = params['T']
+    # Ts = [1.0, 0.5]
+    F = params['pumpA']
+    mu = params['mu']
+    # dts = [0.0125, 0.01, 0.0075, 0.005]
+    # dts = [0.01]
+    dt = 0.01
+    tmax = params['tmax']
+
+    Fs = [0.0, 2.0, 4.0, 8.0]
+    # Us = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0]
+    # Ts = [0.25, 0.5, 1.0]
+    # Ts = [1.0]
+    # mus = [0.0, 1.0, 2.0, 3.0]
+    # mus = [0.0]
+
     for F in Fs:
-        t, I = calculate_current(params['U'], F, params['T'], params['v_0'], params['lattice_structure']) 
-        # plt.plot(t, np.real(I[0] + I[1]), label = 'F = {}'.format(F)) 
-        plt.plot(t, np.real(I[0]), label = 'F = {}'.format(F)) 
-        plt.plot(t, np.real(I[1]), '--') 
-        plt.legend()
-    
-    plt.savefig('current_U={}.pdf'.format(params['U']))
+    # for T in Ts: 
+    #         for mu in mus:
+        t, I = calculate_current(U, F, mu, mu, T, dt, params['v_0'], params['lattice_structure']) 
+        plt.plot(t, np.real(I[0]), label = 'up_I_F={}'.format(F)) 
+        # plt.plot(t, np.real(I[1]), label = 'down_I_F={}'.format(F)) 
+
+    plt.legend()
+    plt.grid()
+    plt.savefig('current_U={}.pdf'.format(U, F))
 
 if __name__ == "__main__":
     main()

calculate_spin_occupation.py
@@ -14,15 +14,14 @@ def tdiff(D, t2, t1):
     """
     return D[t2 - t1] if t2 >= t1 else np.conj(D[t1 - t2])
 
-def calculate_occupation(U, pumpA, T, v_0, lattice_structure):
+def calculate_occupation(U, pumpA, mu1, mu2, T, dt):
     # load Greens functions
-    K = 'K_1_f_U={}_F={}_T={}.npz'
-    loaded = np.load(Green.format(U, pumpA, T))
+    K = 'K_1_f_U={}_F={}_mu1={}_mu2={}_T={}_dt={}.npz'
+    loaded = np.load(K.format(U, pumpA, mu1, mu2, T, dt))
     t = loaded['t']
-    dt = t[1] - t[0]
     K = loaded['K']
 
-    return K.diagonal()
+    return t, K[0].diagonal(), K[1].diagonal(), K[2].diagonal(), K[3].diagonal()
 
 def main():
     parser = argparse.ArgumentParser(description = "calculate current")
@@ -34,11 +33,11 @@ def main():
 
     params.update(vars(args))
     
-    Fs = [0.0]
+    Fs = [0.0, 2.0, 4.0, 8.0, 15.0]
     for F in Fs:
-        K = calculate_occupation(params['U'], F, params['T'], params['v_0'], params['lattice_structure'])
-        plt.plot(t, np.real(K[0] + K[3]), label = 'spin_up_F = {}'.format(F)) 
-        plt.plot(t, np.real(K[1] + K[3]), label = 'spin_down_F = {}'.format(F)) 
+        t, K0, K1, K2, K3 = calculate_occupation(params['U'], F, params['mu'], params['mu'], params['T'], params['dt'])
+        # plt.plot(t, np.real(K0 + K3), label = 'spin_up_F = {}'.format(F)) 
+        plt.plot(t, np.real(K1 + K3), label = 'spin_down_F = {}'.format(F)) 
 
     plt.legend()
     plt.savefig('spin_population.pdf')

dmft.py
@@ -46,7 +46,7 @@ def run_dmft(U, T, pumpA, probeA, mu, v_0, tmax, dt, dw, tol, solver, phonon, fe
     t_contour = np.arange(-tmax, tmax, dt)
     w = np.arange(-wC, wC, dw) 
 
-    msg = 'Starting DMFT loop for U = {} | Temperature = {} | mu = {} | phonon = {} | fermion = {} | Lambda = {} | time = {} | dt = {}'.format(U, T, mu, phonon, fermion, Lambda, tmax, dt)
+    msg = 'Starting DMFT loop for U = {} | F = {} | mu = {} | phonon = {} | fermion = {} | Lambda = {} | time = {} | dt = {}'.format(U, pumpA, mu, phonon, fermion, Lambda, tmax, dt)
     print('-'*len(msg))
     print(msg)
     print('-'*len(msg))

hybridization.py
@@ -3,7 +3,6 @@ from scipy.fftpack import fft, ifft, fftfreq, fftshift, ifftshift
 import matplotlib.pyplot as plt
 
 def tdiff(D, t2, t1):
-
     """
     Create two time object from one time object
     """
@@ -146,39 +145,50 @@ def genSemicircularHyb(T, mu, v_0, wC, tmax, dt, dw):
     w_start = int(N / 2 - int(wC / dw))
     w_end = int(N /2 + int(wC / dw))
 
-    # plt.plot(w[b:a], DOS[b:a], label = 'dos')
-    plt.plot(w, dos[w_start:w_end], label = 'dos')
-    plt.savefig('dos.pdf')
-    plt.close()
+    # plt.plot(w, dos[w_start:w_end], label = 'dos')
+    # plt.savefig('dos.pdf')
+    # plt.close()
 
-    # np.savez_compressed('Delta', t=t, D=Delta)
-    np.savez_compressed('Delta', t=t, dos=dos[w_start:w_end], D=Delta)
+    # plt.plot(w, np.real(Hyb_les[w_start:w_end]), '-', w, np.imag(Hyb_les[w_start:w_end]), '--', label = 'Hyb_les')
+    # plt.plot(w, np.real(Hyb_gtr[w_start:w_end]), '-', w, np.imag(Hyb_gtr[w_start:w_end]), '--', label = 'Hyb_gtr')
+    # plt.show()
+
+    np.savez_compressed('Delta_mu={}_T={}_dt={}'.format(mu, T, dt), t=t, dos=dos[w_start:w_end], D=Delta)
 
 
-def genWideBandHyb(T, mu, tmax, dt, dw):
+def genWideBandHyb(T, mu, wC, tmax, dt, dw):
     """
     Generate Hybridization function for Fermion bath with a wide, flat band
     """
     # additional parameters to generate the flat band
-    wC = 10
-    v = 10
-    gamma = 1
-
+    v = 1.0
+    # gamma = 1.0 / 2.0 * wC
+    gamma = 1.0
     beta = 1.0 / T
     Cut  = np.pi / dt
-    N = int(2*Cut/dw)
+    
+    dw = dt
+    t = np.arange(0, tmax, dt)
+    w = np.arange(-wC, wC, dw)
+    fw = np.arange(-Cut, Cut, dw)
 
-    t    = np.arange(0, tmax, dt)
-    w    = np.arange(-Cut, Cut, dw)
+    N = len(fw)
+    w_start = int(N / 2 - int(wC / dw))
+    w_end = int(N /2 + int(wC / dw))
+    dos = np.zeros(N)
+
+    dos = flatBand(fw, wC, v, gamma)
+    fermi = fermi_function(fw, beta, mu)
 
     Delta = np.zeros((2, 2, len(t), len(t)), complex)  # indices are gtr/les | spin up/spin down
 
     # frequency-domain Hybridization function
-    Hyb_gtr = flatBand(w, wC, v, gamma) * (1 - fermi_function(w, beta, mu))
-    Hyb_les = flatBand(w, wC, v, gamma) * fermi_function(w, beta, mu)
+    Hyb_les = dos  * fermi
+    Hyb_gtr = dos * (1 - fermi)
 
-    fDelta_gtr = ifftshift(fft(fftshift(Hyb_gtr))) * dw/(np.pi)
-    fDelta_les = ifftshift(fft(fftshift(Hyb_les))) * dw/(np.pi)
+    # fDelta_les = np.conj(ifftshift(fft(fftshift(Hyb_les)))) * dw/np.pi
+    fDelta_les = ifftshift(fft(fftshift(Hyb_les))) * dw/np.pi
+    fDelta_gtr = ifftshift(fft(fftshift(Hyb_gtr))) * dw/np.pi
 
     # get real times from fft_times
     Delta_init = np.zeros((2, 2, len(t)), complex)  # greater/lesser | spin up/spin down
@@ -187,11 +197,35 @@ def genWideBandHyb(T, mu, tmax, dt, dw):
         Delta_init[0, :, t1] = fDelta_gtr[int(N / 2) + t1]
         Delta_init[1, :, t1] = fDelta_les[int(N / 2) + t1]
 
-    for t1 in range(len(t)):
-        for t2 in range(len(t)):
+    for t1 in range(len(t)-1,0,-1):
+        for t2 in range(len(t)-1,0,-1):
+    # for t1 in range(len(t)):
+    #     for t2 in range(len(t)):
             Delta[0, 0, t2, t1] = tdiff(Delta_init[0, 0], t2, t1)
             Delta[0, 1, t2, t1] = tdiff(Delta_init[0, 1], t2, t1)
             Delta[1, 0, t1, t2] = tdiff(Delta_init[1, 0], t2, t1)
             Delta[1, 1, t1, t2] = tdiff(Delta_init[1, 1], t2, t1)
 
-    np.savez_compressed('Delta', t=t, D=Delta)
+    t_cut = len(t)
+    # t_cut = 1
+
+    # plt.plot(t[:t_cut], np.real(Delta_init[1, 1, :t_cut]), t[:t_cut], np.imag(Delta_init[1, 1, :t_cut]), '--', label='Delta_les')
+    # plt.plot(t[:t_cut], np.real(Delta_init[0, 1, :t_cut]), t[:t_cut], np.imag(Delta_init[0, 1, :t_cut]), '--', label='Delta_gtr')   
+    # plt.legend()
+    # plt.show()
+
+
+    # plt.plot(t[:t_cut], np.real(Delta[1, 1, :t_cut, t_cut-1]), t[:t_cut], np.imag(Delta[1, 1, :t_cut, t_cut-1]), '--', label='Delta_les')
+    # plt.plot(t[:t_cut], np.real(Delta[1, 1, t_cut-1, :t_cut]), t[:t_cut], np.imag(Delta[1, 1, t_cut-1, :t_cut]), '--', label='Delta_gtr')   
+    # plt.legend()
+    # plt.savefig('Delta_mu={}_T={}_dt={}.pdf'.format(mu, T, dt))
+    # plt.close()
+
+    # plt.plot(fw[w_start:w_end], np.real(Hyb_les[w_start:w_end]), '-', fw[w_start:w_end], np.imag(Hyb_les[w_start:w_end]), '--', label = 'Hyb_les')
+    # plt.plot(fw[w_start:w_end], np.real(Hyb_gtr[w_start:w_end]), '-', fw[w_start:w_end], np.imag(Hyb_gtr[w_start:w_end]), '--', label = 'Hyb_gtr')
+    # plt.legend()
+    # plt.savefig('Hybridization_mu={}_T={}_dt={}.pdf'.format(mu, T, dt))
+    # plt.close()
+    # # plt.show()
+
+    np.savez_compressed('Delta_mu={}_T={}_dt={}'.format(mu, T, dt) , t=t, D=Delta)

nca.py
@@ -60,7 +60,7 @@ def fillDeltaMatrix(DeltaMatrix, Delta):
     DeltaMatrix[3, 1] = Delta[0, 1]
     DeltaMatrix[3, 2] = Delta[0, 0]
 
-def solve(t, U, pumpA, T, G_0, phonon, fermion, Lambda, dissBath, output, Delta):
+def solve(t, U, mu1, mu2, pumpA, T, G_0, phonon, fermion, Lambda, dissBath, output, Delta):
     ########## Computation of bold propagators on separate branches of the contour ##########
     #
     # with h5py.File(output, "r") as h5f:
@@ -240,10 +240,10 @@ def solve(t, U, pumpA, T, G_0, phonon, fermion, Lambda, dissBath, output, Delta)
         # with h5py.File(output, "a") as h5f:
         #     hdf5.save_green(h5f, gfsection, Green[:,:,1,:,:], (t,t))
 
-        Vertexfunction = 'K_1_f_U={}_F={}_T={}'
-        Greensfunction = 'Green_U={}_F={}_T={}'
-        np.savez_compressed(Vertexfunction.format(U, pumpA, T), t=t, K=K[i])
-        np.savez_compressed(Greensfunction.format(U, pumpA, T), t=t, Green=Green[:,:,i])
+        Vertexfunction = 'K_1_f_U={}_F={}_mu1={}_mu2={}_T={}_dt={}'
+        Greensfunction = 'Green_U={}_F={}_mu1={}_mu2={}_T={}_dt={}'
+        np.savez_compressed(Vertexfunction.format(U, pumpA, mu1, mu2, T, dt), t=t, K=K[i])
+        np.savez_compressed(Greensfunction.format(U, pumpA, mu1, mu2, T, dt), t=t, Green=Green[:,:,i])
 
         return Green[:,:,i]
 